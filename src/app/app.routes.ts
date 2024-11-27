5e78df53dcd99efb9057728fc1ddaf5cddc0e3ea
Jasper <jaspermesenbrink@gmail.com>
2024-11-27 16:58:21 -0700

    add page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./core/home/home.component";
 import { QuizComponent } from "./core/quiz/quiz.component";
+import { PageNotFoundComponent } from "./core/page-not-found/page-not-found.component";
 
 export const routes: Routes = [
   { path: "quiz", component: QuizComponent },
   { path: "", component: HomeComponent },
+  { path: "*", component: PageNotFoundComponent }
 ];
