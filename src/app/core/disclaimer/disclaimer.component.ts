fbada418c02a8fd1ef668f10237457eb5bbaac28
Jasper <jaspermesenbrink@gmail.com>
2024-12-03 13:44:03 -0700

    added disclaimer

---
+++ src/app/core/disclaimer/disclaimer.component.ts
@@ -0,0 +1,36 @@
+import {ChangeDetectionStrategy, Component, inject, model, signal} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+@Component({
+  selector: 'app-disclaimer',
+  imports: [MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
+  templateUrl: './disclaimer.component.html',
+  styleUrl: './disclaimer.component.scss'
+})
+export class DisclaimerComponent {
+  readonly dialogRef = inject(MatDialogRef<DisclaimerComponent>);
+
+  close(): void {
+    this.dialogRef.close();
+  }
+}
--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,9 +1,23 @@
-import { Component } from "@angular/core";
+import { AfterViewInit, Component, OnInit } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { RouterModule } from "@angular/router";
 import { ResearchProcessComponent } from "../research-process/research-process.component";
 import { DbService } from "../../shared/services/db.service";
 import { LeaderComponent } from "../../shared/leader/leader.component";
+import {ChangeDetectionStrategy, inject, model, signal} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { DisclaimerComponent } from "../disclaimer/disclaimer.component";
 @Component({
   selector: "app-home",
   imports: [
@@ -11,11 +25,24 @@ import { LeaderComponent } from "../../shared/leader/leader.component";
     RouterModule,
     ResearchProcessComponent,
     ResearchProcessComponent,
-    LeaderComponent
+    LeaderComponent,
   ],
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  readonly dialog = inject(MatDialog);
   constructor(public dbService: DbService) {}
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.openDialog()
+    },2050)
+  }
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DisclaimerComponent );
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 }
